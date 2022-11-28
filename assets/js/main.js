a4c09e6f7ba88617d9fcbcb9eca3294a1a796de6
RaffaelMiguell <raffaelmiguell@gmail.com>
2022-11-28 20:10:24 -0300

    Criando lista dinâmica com js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,48 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function itemShowHtml(pokemon) {
+   return `
+   <li class="pokemon">
+  <span class="number">#001</span>
+  <span class="name">${pokemon.name}</span>
+
+  <div class="detail">
+     <ul class="types">
+        <li class="type">Grass</li>
+        <li class="type">Poison</li>
+     </ul>
+     <img
+        src="./assets/imgs/bulbasaur.png"
+        alt="Pokemon ${pokemon.name}"
+     />
+  </div>
+</li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
-   .then(function (response) {
-    response.json() /* convertendo recurso 'ReadableStream' para .json */
-    console.log(response)
-   })
-   .catch(function (error) {
-      console.error(error)
-   })
-   .finally(function () {
-      console.log('Requisição feita com sucesso.')
-   })
-   
+   .then(response =>
+      response.json()
+   ) /* convertendo recurso 'ReadableStream' para .json */
+   .then(jsonBody => {
+      // debugger
+      return jsonBody.results
+   }) /* após receber response, filtrar "results"*/
+   .then(pokemons => {
+      // debugger
+      for (let i = 0; i < pokemons.length; i++) {
+         const pokemon = pokemons[i]
 
+         pokemonList.innerHTML += itemShowHtml(pokemon)
+      }
+   }) /* criando list para consumo no html */
+   .catch(error => console.error(error))
+   .finally(() => console.log('Requisição feita com sucesso.'))
 
-  //  mesma funcionalidade do try catch
-    
-  //  .then{}
-  //  .catch{}
-  //  .finaly{}
+//  mesma funcionalidade do try catch
+
+//  .then{} é possível encadear mais vezes
+//  .catch{}
+//  .finaly{}
